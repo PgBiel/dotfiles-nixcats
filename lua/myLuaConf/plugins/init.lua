2dce7f21498b89208ebaf56d69913c6f30ec2e04
PgBiel <9021226+PgBiel@users.noreply.github.com>
2025-02-08 19:12:12 -0300

    fix typing 's' on search

--- lua/myLuaConf/plugins/init.lua
+++ lua/myLuaConf/plugins/init.lua
@@ -461,7 +461,7 @@ require('lze').load {
       { "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
       { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
       { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
-      { "s", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
+      { "<leader>zs", mode = "n", function() require("flash").toggle() end, desc = "Toggle Flash Search" },
     },
   },
   {
